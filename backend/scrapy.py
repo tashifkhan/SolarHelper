6083be1668f37209c88b6d5426368346f210bf33
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-15 17:20:26 +0530

    better scapping logic

--- backend/scrapy.py
+++ backend/scrapy.py
@@ -6,18 +6,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def return_md(url):
     res = requests.get(url)
-    soup = BeautifulSoup(res.content, 'html.parser')
+    soup = BeautifulSoup(
+        res.content,
+        "html.parser",
+    )
     markdowntext = html2text.html2text(soup.body.prettify())
     return markdowntext
+
 
 def write_file(filename, markdowntext):
     with open(filename, "w+") as ft:
         ft.write(markdowntext)
         ft.close()
 
+
 def split_dom_content(dom_content, max_length=6000):
     return [
-        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
+        dom_content[i : i + max_length]
+        for i in range(
+            0,
+            len(dom_content),
+            max_length,
+        )
     ]
